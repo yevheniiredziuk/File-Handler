04b75b4d13d9fd75ea4d43bb098a65d59520fc5f
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-07-26 15:01:13 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    private final static String BASE_PATH = "files/";
+
+    public static void main(String[] args) {
+        FileHandler handler = new FileHandler();
+        String fileName = "myfile";
+        String fileContent = "My very important information.";
+        String result = handler.writeFile(fileName, fileContent);
+        String content = handler.readFile(BASE_PATH + fileName + ".txt");
+        getOutput("RESULT: " + result);
+        getOutput("FILE CONTENT: " + content);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
