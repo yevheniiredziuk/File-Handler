ee4a6146d4b73bfae1bc878eeca0ffc4773fe1cb
yevheniiredziuk <yevheniiredziuk@gmail.com>
2025-07-26 15:07:43 +0300

    add FileHandler

---
+++ src/app/FileHandler.java
@@ -0,0 +1,33 @@
+package app;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileHandler {
+
+    private final static String BASE_PATH = "files/";
+
+    public String writeFile(String fileName, String fileContent) {
+
+        try(FileWriter fw = new FileWriter(fileName + ".txt")) {
+            fw.write(fileContent);
+            return "Success.";
+        } catch (Exception e) {
+            return "Fail.";
+        }
+    }
+
+    public String readFile(String path) {
+        try (FileReader reader = new FileReader(path)) {
+            int sym;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((sym = reader.read()) != -1) {
+                stringBuilder.append((char) sym);
+            }
+            return stringBuilder.toString();
+        } catch (IOException ex) {
+            return ex.getMessage();
+        }
+    }
+}
--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,12 @@
+package app;
+
 public class Main {
 
     private final static String BASE_PATH = "files/";
 
     public static void main(String[] args) {
         FileHandler handler = new FileHandler();
-        String fileName = "myfile";
+        String fileName = "myFile";
         String fileContent = "My very important information.";
         String result = handler.writeFile(fileName, fileContent);
         String content = handler.readFile(BASE_PATH + fileName + ".txt");
